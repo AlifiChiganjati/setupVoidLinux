b39ee0d765e80bd65e6d4c90247a8febfbc7c262
Alifi Chiganjati <124354907+AlifiChiganjati@users.noreply.github.com>
2024-02-13 07:14:02 +0700

    Update lazy.lua

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -35,15 +35,21 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = {
+    enabled = true,
+    notify = false,
+  }, -- automatically check for plugin updates
+  change_detection = {
+    notify = false,
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
